76e94b8e119aa6b92b895800f6eb522b21b4702d
Thomas Tuegel <ttuegel@gmail.com>
2015-12-19 20:24:29 -0600

    sigil: Qt 5 infrastructure update

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3025,7 +3025,7 @@ let
 
   siege = callPackage ../tools/networking/siege {};
 
-  sigil = callPackage ../applications/editors/sigil { };
+  sigil = qt5.callPackage ../applications/editors/sigil { };
 
   # aka., gpg-tools
   signing-party = callPackage ../tools/security/signing-party { };
