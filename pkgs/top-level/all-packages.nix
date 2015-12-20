c3d85872ac9749db053f430f912a07162956aa3e
Thomas Tuegel <ttuegel@gmail.com>
2015-12-19 20:44:44 -0600

    linssid: Qt 5 infrastructure update

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11919,7 +11919,7 @@ let
 
   libquvi = callPackage ../applications/video/quvi/library.nix { };
 
-  linssid = callPackage ../applications/networking/linssid { };
+  linssid = qt5.callPackage ../applications/networking/linssid { };
 
   mi2ly = callPackage ../applications/audio/mi2ly {};
 
